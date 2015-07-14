842caf9de24bb015bb703e97cd451fcfc84b14f8
think <think@Lenovo-PC.mshome.net>
2015-07-14 17:27:45 +0800

    commit

---
+++ test/MybatisSprintTest.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+import logistics.LogisticsMessage;
+import mybatis.inter.LogisticsMessageOperation;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MybatisSprintTest { 
+	protected static Log logger = LogFactory.getLog(MybatisSprintTest.class);
+    private static ApplicationContext ctx;  
+      
+    static    
+    {    
+        ctx = new ClassPathXmlApplicationContext("config/applicationContext.xml"); 
+    }         
+         
+    public static void main(String[] args)    
+    {  
+    	 logger.info(ctx); 
+    	LogisticsMessageOperation mapper = (LogisticsMessageOperation)ctx.getBean("LogisticsMessage"); 
+        //测试id=1的用户查询，根据数据库中的情况，可以改成你自己的.
+        System.out.println("得到用户id=1的用户信息");
+        List<LogisticsMessage> users = mapper.selectUsers("");
+        logger.info(users);
+    }  
+
+    
+}
