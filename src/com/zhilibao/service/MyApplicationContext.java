08789fa1bfdaebeb17577bac8425ec6217afc43c
think <think@Lenovo-PC.mshome.net>
2015-07-19 15:48:10 +0800

    提交

--- src/com/zhilibao/service/MyApplicationContext.java
+++ src/com/zhilibao/service/MyApplicationContext.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApplicationContext { 
-          
+           
     public static ApplicationContext getInstance(){
     	return  new ClassPathXmlApplicationContext("ApplicationContext.xml");   
     } 
